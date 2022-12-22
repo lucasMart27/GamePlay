8253cd98604e88dc0da8470fd9ae1162b89d1dae
“lucasMart27” <lucas_mart27@hotmail.com>
2022-12-21 23:59:01 -0300

    'Construção SignIn

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,16 +1,25 @@
 import React, { useState } from "react";
-import { View, Text, TextInput } from "react-native";
+import { View, Text, Image } from "react-native";
 
+import IllustrationImg from "../../assets/illustration.png";
 import { styles } from "./styles";
 
 export function SignIn() {
   const [text, setText] = useState("");
   return (
     <View style={styles.conntainer}>
-      <Text>Hello Word</Text>
-
-      <TextInput style={styles.input} onChangeText={setText} />
-      <Text>Você digitou: {text}</Text>
+      <Image source={IllustrationImg} style={styles.image} />
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize {"\n"}
+          suas jogatinas {"\n"}
+          facilmente{"\n"}
+        </Text>
+        <Text>
+          Crie grupos para jogar seus games {"\n"}
+          favooritos com seus aigoos
+        </Text>
+      </View>
     </View>
   );
 }
