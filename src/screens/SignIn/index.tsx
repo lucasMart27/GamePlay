01ff5a1feab6760834b9f21a36c90618cb8248c7
“lucasMart27” <lucas_mart27@hotmail.com>
2022-12-22 00:43:54 -0300

    'Construção SignIn part2

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,21 +1,32 @@
 import React, { useState } from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, StatusBar } from "react-native";
 
 import IllustrationImg from "../../assets/illustration.png";
 import { styles } from "./styles";
 
 export function SignIn() {
-  const [text, setText] = useState("");
   return (
     <View style={styles.conntainer}>
-      <Image source={IllustrationImg} style={styles.image} />
+      <StatusBar 
+      barStyle={"light-content"} 
+      backgroundColor="transparent"
+      translucent
+      />
+
+      <Image
+        source={IllustrationImg}
+        style={styles.image}
+        resizeMode="stretch"
+      />
+
       <View style={styles.content}>
         <Text style={styles.title}>
           Organize {"\n"}
           suas jogatinas {"\n"}
           facilmente{"\n"}
         </Text>
-        <Text>
+
+        <Text style={styles.subtitle}>
           Crie grupos para jogar seus games {"\n"}
           favooritos com seus aigoos
         </Text>
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,3 +1,4 @@
+import { theme } from "./../../global/styles/theme";
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
@@ -5,10 +6,27 @@ export const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: theme.colors.background,
   },
-  input: {
-    height: 50,
-    width: 200,
-    borderBottomWidth: 2,
+  image: {
+    width: "100%",
+    height: 360,
+  },
+  content: {
+    marginTop: -40,
+  },
+
+  title: {
+    color: theme.colors.heading,
+    textAlign: "center",
+    fontSize: 40,
+    marginBottom: 16,
+  },
+
+  subtitle: {
+    color: theme.colors.heading,
+    fontSize: 15,
+    textAlign: "center",
+    marginBottom: 64,
   },
 });
