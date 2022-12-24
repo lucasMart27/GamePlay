8e38ccf7b38e9a62244c27c14b7c2c49667eafc1
“lucasMart27” <lucas_mart27@hotmail.com>
2022-12-23 23:35:45 -0300

    Novas Fonts

--- App.tsx
+++ App.tsx
@@ -2,7 +2,27 @@ import React from "react";
 import { SignIn } from "./src/screens/SignIn";
 import { StatusBar } from "expo-status-bar";
 import { useFonts } from "expo-font";
+import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
+
+import {
+  Rajdhani_500Medium,
+  Rajdhani_700Bold,
+} from "@expo-google-fonts/rajdhani";
+
+import AppLoading from "expo-app-loading";
+("expo-app-loading");
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return <SignIn />;
 }
