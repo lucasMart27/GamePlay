900b1bcc2c95ba594dbce66514a74a50a34476f8
Lucas Martins <lucasmartins@MacBook-Pro.local>
2023-01-16 23:56:30 -0300

    react-native-iphone-x-helper

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { View } from "react-native";
+import { styles } from "./styles";
 
 export function Home() {
-  return <View></View>;
+  return (
+    <View>
+      <View style={StyleSheet.header}></View>
+    </View>
+  );
 }
