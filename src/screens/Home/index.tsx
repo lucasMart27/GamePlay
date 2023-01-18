26054d83108895ad619be90bb2901e6ebff5de1a
Lucas Martins <lucasmartins@MacBook-Pro.local>
2023-01-17 23:44:36 -0300

    Tela Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { View } from "react-native";
+import { Profile } from "../../components/Profile";
 import { styles } from "./styles";
 
 export function Home() {
   return (
     <View>
-      <View style={StyleSheet.header}></View>
+      <View style={styles.header}>
+        <Profile />
+      </View>
     </View>
   );
 }
