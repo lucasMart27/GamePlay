b6ccc366a0e867b079992159994a91e5127e1644
Lucas Martins <lucasmartins@MacBook-Pro.local>
2023-01-23 16:11:32 -0300

    construcao do avatar

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -2,15 +2,21 @@ import React from "react";
 import { View, Text } from "react-native";
 import { styles } from "./styles";
 
-export function Profile() {
-  <View style={styles.container}>
-    <View>
-      <View style={styles.user}>
-        <Text stylo={styles.greeting}>Olá,</Text>
+import { Avatar } from "../Avatar";
 
-        <Text styles={styles.username}>Lucas</Text>
+export function Profile() {
+  return (
+    <View style={styles.container}>
+      <Avatar urlImage="https://github.com/lucasMart27.png" />
+
+      <View>
+        <View style={styles.user}>
+          <Text style={styles.greeting}>Olá,</Text>
+
+          <Text style={styles.username}>Lucas</Text>
+        </View>
+        <Text style={styles.message}>Hoje é dia vitória</Text>
       </View>
-      <Text style={styles.nessage}>Hoje é dia vitória</Text>
     </View>
-  </View>;
+  );
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { View } from "react-native";
 import { Profile } from "../../components/Profile";
+
 import { styles } from "./styles";
 
 export function Home() {
