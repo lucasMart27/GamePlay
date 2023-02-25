df6c2b5fa6fe7ebcdcbb33fdf6a1f068b06810aa
Lucas Martins <lucasmartins@MacBook-Pro-3.local>
2023-02-24 23:51:36 -0300

    ListDivider2

--- src/components/ListDivider/styles.ts
+++ src/components/ListDivider/styles.ts
@@ -1,7 +1,12 @@
+import { theme } from "./../../global/styles/theme";
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
-    flex: 1
+    width: "80",
+    height: 1,
+    backgroundColor: theme.colors.secondary40,
+    marginVertical: 21,
+    alignSelf: "flex-end"
   }
 });
