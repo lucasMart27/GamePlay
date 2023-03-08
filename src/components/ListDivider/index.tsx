88b7a47b860de07278d521bb92afee994ccd55aa
Lucas Martins <lucasmartins@MacBook-Pro-3.local>
2023-03-07 23:43:17 -0300

    ListDivider atualizacao

--- src/components/ListDivider/index.tsx
+++ src/components/ListDivider/index.tsx
@@ -1,8 +1,26 @@
 import React from "react";
-
 import { View } from "react-native";
+
 import { styles } from "./styles";
 
-export function ListDivider() {
-  return <View></View>;
+type Props = {
+  isCentered?: boolean;
+};
+
+export function ListDivider({ isCentered }: Props) {
+  return (
+    <View
+      style={[
+        styles.container,
+        isCentered
+          ? {
+              marginVertical: 12
+            }
+          : {
+              marginTop: 2,
+              marginBottom: 31
+            }
+      ]}
+    />
+  );
 }
