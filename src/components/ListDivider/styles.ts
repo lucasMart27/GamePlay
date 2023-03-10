9726b0929c36deeeffc7ca6814069fa4d17bef96
Lucas Martins <lucasmartins@MacBook-Pro-3.local>
2023-03-09 23:57:27 -0300

    ListHeader atualizacao

--- src/components/ListDivider/styles.ts
+++ src/components/ListDivider/styles.ts
@@ -3,9 +3,20 @@ import { theme } from "../../global/styles/theme";
 
 export const styles = StyleSheet.create({
   container: {
-    height: 1,
-    width: "78%",
-    alignSelf: "flex-end",
-    backgroundColor: theme.colors.secondary40
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 24,
+    marginTop: 27
+  },
+  title: {
+    fontFamily: theme.fonts.title700,
+    color: theme.colors.heading,
+    fontSize: 18
+  },
+  subtitle: {
+    fontFamily: theme.fonts.text400,
+    color: theme.colors.highlight,
+    fontSize: 13
   }
 });
