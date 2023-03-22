d731cf00a5a744c2e4c93c241857a080d1a98cb1
Lucas Martins <lucasmartins@MacBook-Pro-3.local>
2023-03-21 23:57:30 -0300

    Guildlcon atualização

--- src/components/GuildIcon/index.tsx
+++ src/components/GuildIcon/index.tsx
@@ -1,26 +1,11 @@
 import React from "react";
-import { Image, View } from "react-native";
+import { Image } from "react-native";
 
 import { styles } from "./styles";
-import DiscordSvg from "../../assets/discord.svg";
 
-const { CDN_IMAGE } = process.env;
+export function GuildIcon() {
+  const uri =
+    "https://gamerssuffice.com/wp-content/uploads/2019/11/How-to-add-bots-to-discord-500x405.jpg";
 
-type Props = {
-  guildId: string;
-  iconId: string | null;
-};
-
-export function GuildIcon({ guildId, iconId }: Props) {
-  const uri = `${CDN_IMAGE}/icons/${guildId}/${iconId}.png`;
-
-  return (
-    <View style={styles.container}>
-      {iconId ? (
-        <Image source={{ uri }} style={styles.image} resizeMode="cover" />
-      ) : (
-        <DiscordSvg width={40} height={40} />
-      )}
-    </View>
-  );
+  return <Image source={{ uri }} style={styles.image} resizeMode="cover" />;
 }
