331c11099c0ecc38567312caa7d5b918e946edaf
Lucas Damasceno <lucasdamasceno@MAC.local>
2023-09-15 15:10:41 -0300

    configuracao servidor

--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -13,6 +13,7 @@ import { styles } from "./styles";
 import { CategorySelect } from "../../components/CategorySelect";
 import { Background } from "../../components/Background";
 import { Header } from "../../components/Header";
+import { GuildIcon } from "../../components/GuildIcon";
 
 export function AppointmentCreate() {
   const [category, setCategory] = useState("");
@@ -20,7 +21,9 @@ export function AppointmentCreate() {
     <Background>
       <Header title="Agendar partida" />
 
-      <Text style={styles.label}>Categoria</Text>
+      <Text style={[styles.label, { marginLeft: 24, marginTop: 36 }]}>
+        Categoria
+      </Text>
       <CategorySelect
         hasCheckBox
         setCategory={setCategory}
@@ -29,12 +32,18 @@ export function AppointmentCreate() {
       <View style={styles.form}>
         <RectButton>
           <View style={styles.select}>
-            <View style={styles.image} />
-
+            {
+              //<View style={styles.image} /> 0:58:31 <---------------------------------------
+              <GuildIcon />
+            }
             <View style={styles.selectBody}>
               <Text style={styles.label}>Selecione um servidor</Text>
             </View>
-            <Feather />
+            <Feather
+              name="chevron-right"
+              color={theme.colors.heading}
+              size={18}
+            />
           </View>
         </RectButton>
       </View>
