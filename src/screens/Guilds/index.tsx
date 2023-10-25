1dbe6e0f7f111e62d1687fdab83fb04dff5692c9
Lucas Martins <lucas_mart27@hotmail.com>
2023-10-24 23:57:14 -0300

    correcao

--- src/screens/Guilds/index.tsx
+++ src/screens/Guilds/index.tsx
@@ -20,24 +20,6 @@ export function Guilds({ handleGuildSelect }: Props) {
       icon: "image.png",
       owner: true,
     },
-    {
-      id: "2",
-      name: "Galera do Game",
-      icon: "image.png",
-      owner: true,
-    },
-    {
-      id: "3",
-      name: "Galera do Game",
-      icon: "image.png",
-      owner: true,
-    },
-    {
-      id: "4",
-      name: "Galera do Game",
-      icon: "image.png",
-      owner: true,
-    },
   ];
   return (
     <View style={styles.container}>
